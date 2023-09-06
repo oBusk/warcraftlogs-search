049626e442de4f79f88adb9302051c7bb03313e5
Oscar Busk <oscar.busk@gmail.com>
2023-09-06 23:58:50 +0200

    Fix hydration issue with table

--- src/components/Rankings.tsx
+++ src/components/Rankings.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import getRankings from "^/lib/rankings";
 import { buildWclUrl } from "^/lib/utils";
 
@@ -33,28 +34,48 @@ export default async function Rankings({
                             spec,
                             amount,
                             report: { code, fightID },
-                        }) => (
-                            <a
-                                key={code + name}
-                                href={buildWclUrl({
-                                    code,
-                                    fightID,
-                                })}
-                                className="table-row hover:bg-gray-100"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <td className="text-left">{name}</td>
-                                <td className="text-left">
-                                    {guild?.name ?? null}
+                        }) => {
+                            const Cell = ({
+                                children,
+                                className,
+                            }: {
+                                children: ReactNode;
+                                className?: string;
+                            }) => (
+                                <td className={className}>
+                                    <a
+                                        href={buildWclUrl({
+                                            code,
+                                            fightID,
+                                        })}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="block"
+                                    >
+                                        {children}
+                                    </a>
                                 </td>
-                                <td className="text-left">{wowClass}</td>
-                                <td className="text-left">{spec}</td>
-                                <td className="text-right font-mono">
-                                    {Math.round(amount).toLocaleString()}
-                                </td>
-                            </a>
-                        ),
+                            );
+
+                            return (
+                                <tr
+                                    key={code + name}
+                                    className="hover:bg-gray-100"
+                                >
+                                    <Cell className="text-left">{name}</Cell>
+                                    <Cell className="text-left">
+                                        {guild?.name ?? null}
+                                    </Cell>
+                                    <Cell className="text-left">
+                                        {wowClass}
+                                    </Cell>
+                                    <Cell className="text-left">{spec}</Cell>
+                                    <Cell className="text-right font-mono">
+                                        {Math.round(amount).toLocaleString()}
+                                    </Cell>
+                                </tr>
+                            );
+                        },
                     )}
                 </tbody>
             </table>
