0591dec4ac8713d2d5db242dddf9e6a6ac605c21
Oscar Busk <oscar.busk@gmail.com>
2023-09-07 13:20:07 +0200

    Improve rankings to not implode on empty results

--- src/components/Rankings.tsx
+++ src/components/Rankings.tsx
@@ -19,7 +19,7 @@ export default async function Rankings({
     className,
     talent,
 }: RankingsProps) {
-    const rankings = await getRankings(
+    const { rankings, count } = await getRankings(
         encounter,
         partition,
         klass,
@@ -29,70 +29,85 @@ export default async function Rankings({
 
     return (
         <div className={className}>
-            <table className="w-full">
-                <thead>
-                    <tr>
-                        <th className="text-left">Name</th>
-                        <th className="text-left">Guild</th>
-                        <th className="text-left">Class</th>
-                        <th className="text-left">Spec</th>
-                        <th className="text-right">DPS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rankings.rankings.map(
-                        ({
-                            name,
-                            guild,
-                            class: wowClass,
-                            spec,
-                            amount,
-                            report: { code, fightID },
-                        }) => {
-                            const Cell = ({
-                                children,
-                                className,
-                            }: {
-                                children: ReactNode;
-                                className?: string;
-                            }) => (
-                                <td className={className}>
-                                    <a
-                                        href={buildWclUrl({
-                                            code,
-                                            fightID,
-                                        })}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        className="block"
-                                    >
-                                        {children}
-                                    </a>
-                                </td>
-                            );
+            {count != null && (
+                <p className="text-xl font-bold mb-2 text-center">
+                    {count} results
+                </p>
+            )}
+            {rankings?.length > 0 ? (
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th className="text-left">Name</th>
+                            <th className="text-left">Guild</th>
+                            <th className="text-left">Class</th>
+                            <th className="text-left">Spec</th>
+                            <th className="text-right">DPS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rankings.map(
+                            ({
+                                name,
+                                guild,
+                                class: wowClass,
+                                spec,
+                                amount,
+                                report: { code, fightID },
+                            }) => {
+                                const Cell = ({
+                                    children,
+                                    className,
+                                }: {
+                                    children: ReactNode;
+                                    className?: string;
+                                }) => (
+                                    <td className={className}>
+                                        <a
+                                            href={buildWclUrl({
+                                                code,
+                                                fightID,
+                                            })}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            className="block"
+                                        >
+                                            {children}
+                                        </a>
+                                    </td>
+                                );
 
-                            return (
-                                <tr
-                                    key={code + name}
-                                    className="hover:bg-gray-100 dark:hover:bg-gray-800"
-                                >
-                                    <Cell className="text-left">{name}</Cell>
-                                    <Cell className="text-left">
-                                        {guild?.name ?? null}
-                                    </Cell>
-                                    <Cell className="text-left">
-                                        {wowClass}
-                                    </Cell>
-                                    <Cell className="text-left">{spec}</Cell>
-                                    <Cell className="text-right font-mono">
-                                        {Math.round(amount).toLocaleString()}
-                                    </Cell>
-                                </tr>
-                            );
-                        },
-                    )}
-                </tbody>
-            </table>
+                                return (
+                                    <tr
+                                        key={code + name}
+                                        className="hover:bg-gray-100 dark:hover:bg-gray-800"
+                                    >
+                                        <Cell className="text-left">
+                                            {name}
+                                        </Cell>
+                                        <Cell className="text-left">
+                                            {guild?.name ?? null}
+                                        </Cell>
+                                        <Cell className="text-left">
+                                            {wowClass}
+                                        </Cell>
+                                        <Cell className="text-left">
+                                            {spec}
+                                        </Cell>
+                                        <Cell className="text-right font-mono">
+                                            {Math.round(
+                                                amount,
+                                            ).toLocaleString()}
+                                        </Cell>
+                                    </tr>
+                                );
+                            },
+                        )}
+                    </tbody>
+                </table>
+            ) : (
+                <h1>No results</h1>
+            )}
         </div>
     );
 }
