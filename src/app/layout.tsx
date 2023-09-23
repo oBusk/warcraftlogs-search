99389b99f554124e090ee104a86c337d9522e86a
Oscar Busk <oscar.busk@gmail.com>
2023-09-23 02:44:19 +0200

    Fix hydration issue in header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,31 +20,32 @@ export default async function RootLayout({
     return (
         <html lang="en">
             <body>
-                <header className="flex p-4 space-x-2 justify-between items-center">
+                <header className="flex p-4 space-x-2 items-end">
                     <Link href="/" className="flex items-end gap-1">
                         <h1 className="text-4xl font-semibold">
                             Warcraftlogs Search
                         </h1>
-                        <small>
-                            <a
-                                href="https://discordapp.com/users/141461759474139136"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-blue-500"
-                            >
-                                nullDozzer
-                            </a>{" "}
-                            on{" "}
-                            <a
-                                href="https://discord.gg/NwGNKdTk"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-blue-500"
-                            >
-                                WoWAnalyzer Discord
-                            </a>
-                        </small>
                     </Link>
+                    <small>
+                        <a
+                            href="https://discordapp.com/users/141461759474139136"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-500"
+                        >
+                            nullDozzer
+                        </a>{" "}
+                        on{" "}
+                        <a
+                            href="https://discord.gg/NwGNKdTk"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-500"
+                        >
+                            WoWAnalyzer Discord
+                        </a>
+                    </small>
+                    <div className="flex-1" />
                     <a
                         href="https://github.com/oBusk/warcraftlogs-search"
                         target="_blank"
