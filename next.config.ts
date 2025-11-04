599de83febee26734945aedaa6cd38e355fa01dd
Oscar Busk <oscar.busk@gmail.com>
2025-11-05 00:21:42 +0100

    Update next/ts configs

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        staleTimes: {
-            dynamic: 60,
-            static: 180,
-        },
-    },
     logging: {
         fetches: {
             fullUrl: true,
